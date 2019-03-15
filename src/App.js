904e1170268d012b18a76ddef271ce7af6fbf62b
Alessia <alessia.rossini.91@gmail.com>
2019-03-15 18:01:52 +0000

    added comparison between typed value from input and anime title(game core)

--- src/App.js
+++ src/App.js
@@ -13,24 +13,24 @@ class App extends React.Component {
     this.state = {
       title: '',
       image: {},
+      inputValue: '',
       hasError: false
     }
-
-
     this.nextTitle = this.nextTitle.bind(this)
     this.handleResponse = this.handleResponse.bind(this)
     this.handleData = this.handleData.bind(this)
     this.handleError = this.handleError.bind(this)
   }
+
   componentDidMount() {
-    console.log("called")
+    console.log("called");
     this.nextTitle();
   }
+
   nextTitle() {
     var variables = {
       id: Math.floor(Math.random() * 9000) + 1
     };
-
     var url = 'https://graphql.anilist.co',
       options = {
         method: 'POST',
@@ -52,17 +52,14 @@ class App extends React.Component {
     return response.json().then(function (json) {
       return response.ok ? json : Promise.reject(json);
     });
-
   }
 
   handleData(data) {
-    console.log(data)
     var title = data.data.Media.title.native;
     var img = data.data.Media.coverImage.large;
     if (title === null) {
       this.nextTitle()
     }
-    // check and if not call again this.nextTitle
     const newStyle = {
       backgroundImage: `url(${img})`
     };
@@ -80,44 +77,57 @@ class App extends React.Component {
     }
   }
 
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+compareTitles =(e) => {
+  e.preventDefault();
+    this.setState({
+      inputValue: this.state.inputValue
+    })
+    var inputValue = this.state.inputValue;
+    var titleNative = this.state.title;
+    console.log(inputValue)
+    console.log(titleNative)
+    if (inputValue === titleNative) {
+      console.log("you guessed it")
+    } else {
+      console.log("you wrote it wrong")
+    }
+}
 
   render() {
-    const {
-      image,
-      title
-    } = this.state;
-    console.log(title)
-    const {
-      editable
-    } = this.props;
-    return ( < section >
-        <
-        form >
-        <
-        input type = "radio"
-        name = "Anime"
-        value = "Anime" / > Anime <
-        input type = "radio"
-        name = "Manga"
-        value = "Manga" / > Manga <
-        /form>  <
-        button onClick = {
-          this.nextTitle
-        } >> < /button> {
-        title && ( < > < div class = "loader" > < div class = "gameImg"
-          style = {
-            image
-          } > < /div></div >
-          <
-          input / >
-          <
-          div > {
-            title
-          } < /div></ > )
-      } <
-      /section>
-  )
-}
+    const {image,title} = this.state;
+    return ( 
+    <section class="flex flex-column align-center">
+        <form >
+        <input type = "radio" name = "Anime" value = "Anime" /> Anime 
+        <input type = "radio" name = "Manga" value = "Manga" /> Manga 
+        </form>  
+        <i class="fa fa-cog fa-spin fa-3x fa-fw"></i>
+        <div class="flex flex-row align-center">
+          {title && 
+            ( < > 
+              < div class = "loader" >
+                < div class = "gameImg" style = {image} > </div> 
+              </div >  
+              <button onClick = {this.nextTitle} >
+              <i class="fas fa-angle-right"></i>
+              </button>               
+              </ > )} 
+        </div> 
+        <form class="flex flex-row">
+            <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+            <button onClick={(e) => this.compareTitles(e)}>Check</button>
+        </form>
+                
+        <div> {title} </div>
+    </section>
+    )
+  }
 }
 
 export default App;
